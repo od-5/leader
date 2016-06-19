b3df646d6ab6559e3b5a0fec398a0f6978089a29
alexy <od-5@yandex.ru>
2016-06-19 09:01:27 +0300

    fix coord

--- apps/ticket/models.py
+++ apps/ticket/models.py
@@ -17,20 +17,20 @@ class Ticket(Common):
     def __unicode__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        # if not self.country and self.city and self.time_zone:
-        try:
-            api_key = settings.HTMLWEB_API_KEY
-            data = getphoneObject(self.phone, api_key)
-            try:
-                self.country = data['fullname']
-                self.city = data['0']['name']
-                self.time_zone = data['time_zone']
-            except:
-                self.country = data['0']['country']
-        except:
-            pass
-        super(Ticket, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     # if not self.country and self.city and self.time_zone:
+    #     try:
+    #         api_key = settings.HTMLWEB_API_KEY
+    #         data = getphoneObject(self.phone, api_key)
+    #         try:
+    #             self.country = data['fullname']
+    #             self.city = data['0']['name']
+    #             self.time_zone = data['time_zone']
+    #         except:
+    #             self.country = data['0']['country']
+    #     except:
+    #         pass
+    #     super(Ticket, self).save(*args, **kwargs)
 
     def performed_at(self):
         pass
