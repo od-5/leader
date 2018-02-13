41682cdca35c635ddceb79ce256d9e663573166f
alexy <od-5@yandex.ru>
2018-02-13 13:00:10 +0300

    fix environment

--- cms/settings/__init__.py
+++ cms/settings/__init__.py
@@ -5,3 +5,11 @@ from .apps import *
 from .middleware import *
 from .other import *
 from .suit import *
+
+try:
+    LOCAL_SETTINGS
+except NameError:
+    try:
+        from ..local_settings import * # noqa
+    except ImportError:
+        pass
